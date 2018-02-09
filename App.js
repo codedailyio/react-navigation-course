46f8b8b8bb64964b7ce1e882fdea649c921edcb9
Jason Brown <browniefed@gmail.com>
2018-02-08 20:00:35 -0800

    Prettier

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-
-      </View>
-    );
+    return <View style={styles.container} />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
 });
