37212077528fde169c0535f97356ada1a010b975
Jason Brown <browniefed@gmail.com>
2018-02-09 19:48:20 -0800

    Add in code

--- App.js
+++ App.js
@@ -1,9 +1,60 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
+import { DrawerNavigator, DrawerItems } from "react-navigation";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+        <Text>Go To Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
+        <Text>Open Drawer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Go to Home</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("DrawerOpen")}>
+        <Text>Open Drawer</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const CustomDrawerContentComponent = props => (
+  <ScrollView style={{ flex: 1, paddingTop: 40, }}>
+    <TouchableOpacity onPress={() => props.navigation.navigate("DrawerClose")}>
+      <Text>Close Drawer</Text>
+    </TouchableOpacity>
+    <DrawerItems {...props} />
+  </ScrollView>
+);
+
+const RootNavigator = DrawerNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Profile: {
+      screen: ProfileScreen,
+    },
+  },
+  {
+    contentComponent: CustomDrawerContentComponent,
+    gesturesEnabled: true,
+  },
+);
 
 export default class App extends React.Component {
   render() {
-    return <View style={styles.container} />;
+    return <RootNavigator />;
   }
 }
 
@@ -11,5 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
