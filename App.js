5a29d518e3dc1dfcc9f6a1d68bea6959c7fbf49f
Jason Brown <browniefed@gmail.com>
2018-02-11 19:05:53 -0800

    Add in modal view

--- App.js
+++ App.js
@@ -1,9 +1,70 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StackNavigator } from "react-navigation";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+        <Text>Go To Profile</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Modal")}>
+        <Text>Open Modal</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("Modal")}>
+        <Text>Open Modal</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const ModalScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Text>Go Back From Modal</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const MainNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Profile: {
+    screen: ProfileScreen,
+  },
+});
+
+const ModalNavigator = StackNavigator(
+  {
+    Main: {
+      screen: MainNavigator,
+    },
+    Modal: {
+      screen: ModalScreen,
+    },
+  },
+  {
+    headerMode: "none",
+    mode: "modal",
+  },
+);
 
 export default class App extends React.Component {
   render() {
-    return <View style={styles.container} />;
+    return <ModalNavigator />;
   }
 }
 
@@ -11,5 +72,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
